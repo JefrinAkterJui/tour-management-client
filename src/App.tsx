f47a72dc390fe789a7542c7443d8b8a3c2a0c3e1
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-08-04 23:49:55 +0600

    all folder setup done

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router"
+import CommonLayout from "./components/Layouts/CommonLayout"
+
+
+function App() {
+
+  return (
+    <>
+      <CommonLayout>
+        <Outlet/>
+      </CommonLayout>
+    </>
+  )
+}
+
+export default App
